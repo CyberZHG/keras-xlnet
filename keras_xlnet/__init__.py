602973a69857d893aa64627f3928d6b938ad4dc9
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2019-07-16 00:47:56 +0800

    Fix eager execution

--- keras_xlnet/__init__.py
+++ keras_xlnet/__init__.py
@@ -2,5 +2,6 @@ from .permutation import *
 from .mask_embed import *
 from .segment_bias import *
 from .segment_embed import *
+from .attention import *
 from .xlnet import *
 from .loader import *
