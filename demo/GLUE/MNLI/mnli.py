9355f8bdb863658ad9f61afc517539ef69f298f8
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2019-08-03 13:36:26 +0800

    Add MNLI demo

--- demo/GLUE/MNLI/mnli.py
+++ demo/GLUE/MNLI/mnli.py
@@ -9,13 +9,20 @@ from keras_xlnet import PretrainedList, get_pretrained_paths
 from keras_xlnet import Tokenizer, load_trained_model_from_checkpoint, ATTENTION_TYPE_BI
 
 EPOCH = 5
-BATCH_SIZE = 8
-SEQ_LEN = 210
-MODEL_NAME = 'QQP.h5'
+BATCH_SIZE = 16
+SEQ_LEN = 100
+MODEL_NAME = 'MNLI.h5'
+
+CLASSES = {
+    'neutral': 0,
+    'entailment': 1,
+    'contradiction': 2,
+}
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 train_path = os.path.join(current_path, 'train.tsv')
-dev_path = os.path.join(current_path, 'dev.tsv')
+dev_matched_path = os.path.join(current_path, 'dev_matched.tsv')
+dev_mismatched_path = os.path.join(current_path, 'dev_mismatched.tsv')
 
 paths = get_pretrained_paths(PretrainedList.en_cased_base)
 tokenizer = Tokenizer(paths.vocab)
@@ -40,14 +47,14 @@ def generate_sequence(path):
     tokens, classes = [], []
     df = pd.read_csv(path, sep='\t', error_bad_lines=False)
     for _, row in df.iterrows():
-        text_a, text_b, cls = row['question1'], row['question2'], row['is_duplicate']
-        if not isinstance(text_a, str) or not isinstance(text_b, str) or cls not in [0.0, 1.0]:
+        text_a, text_b, cls = row['sentence1'], row['sentence2'], row['gold_label']
+        if not isinstance(text_a, str) or not isinstance(text_b, str) or cls not in CLASSES:
             continue
-        encoded_a, encoded_b = tokenizer.encode(text_a)[:100], tokenizer.encode(text_b)[:100]
+        encoded_a, encoded_b = tokenizer.encode(text_a)[:48], tokenizer.encode(text_b)[:49]
         encoded = encoded_a + [tokenizer.SYM_SEP] + encoded_b + [tokenizer.SYM_SEP]
         encoded = [tokenizer.SYM_PAD] * (SEQ_LEN - 1 - len(encoded)) + encoded + [tokenizer.SYM_CLS]
         tokens.append(encoded)
-        classes.append(int(cls))
+        classes.append(CLASSES[cls])
     tokens, classes = np.array(tokens), np.array(classes)
     segments = np.zeros_like(tokens)
     segments[:, -1] = 1
@@ -57,7 +64,8 @@ def generate_sequence(path):
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 train_seq = generate_sequence(train_path)
-dev_seq = generate_sequence(dev_path)
+dev_matched_seq = generate_sequence(dev_matched_path)
+dev_mismatched_seq = generate_sequence(dev_mismatched_path)
 
 
 # Load pretrained model
@@ -76,7 +84,7 @@ model = load_trained_model_from_checkpoint(
 last = Extract(index=-1, name='Extract')(model.output)
 dense = keras.layers.Dense(units=768, activation='tanh', name='Dense')(last)
 dropout = keras.layers.Dropout(rate=0.1, name='Dropout')(dense)
-output = keras.layers.Dense(units=2, activation='softmax', name='Softmax')(dropout)
+output = keras.layers.Dense(units=3, activation='softmax', name='Softmax')(dropout)
 model = keras.models.Model(inputs=model.inputs, outputs=output)
 model.summary()
 
@@ -93,7 +101,7 @@ model.compile(
 
 model.fit_generator(
     generator=train_seq,
-    validation_data=dev_seq,
+    validation_data=dev_matched_seq,
     epochs=EPOCH,
     callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', patience=2)],
 )
@@ -102,25 +110,6 @@ model.save_weights(MODEL_NAME)
 
 # Evaluation
 # Use dev set because the results of test set is unknown
-results = model.predict_generator(dev_seq, verbose=True).argmax(axis=-1)
-tp, fp, fn, tn = 0, 0, 0, 0
-for i in range(len(results)):
-    if results[i] == 1:
-        if dev_seq.y[i] == 1:
-            tp += 1
-        else:
-            fp += 1
-    else:
-        if dev_seq.y[i] == 1:
-            fn += 1
-        else:
-            tn += 1
-
-print('Confusion:')
-print('[{}, {}]'.format(tp, fp))
-print('[{}, {}]'.format(fn, tn))
-
-print('Accuracy: %.2f' % (100.0 * (tp + tn) / len(results)))
-print('Precision: %.2f' % (100.0 * tp / (tp + fp)))
-print('Recall: %.2f' % (100.0 * tp / (tp + fn)))
-print('F1-Score: %.2f' % (100.0 * (2.0 * tp) / (2.0 * tp + fp + fn)))
+for dev_seq in [dev_matched_seq, dev_mismatched_seq]:
+    results = model.predict_generator(dev_seq, verbose=True).argmax(axis=-1)
+    print('Accuracy: %.2f' % (100.0 * np.sum(results == dev_seq.y) / len(results)))
